a0aff716d36d339f0b5c7fc9e26ba638fe82c2e0
Azure <nadamustafa1995mm@gmail.com>
2026-02-18 14:09:41 +0000

    Create script.js for JavaScript functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript functionality for the portfolio website
+
+// Function to initialize the portfolio
+function initPortfolio() {
+    console.log('Portfolio Initialized');
+    // Other initialization code goes here
+}
+
+// Call the init function when the window loads
+window.onload = initPortfolio;
